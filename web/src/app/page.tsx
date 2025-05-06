1b61174003fca2cc276c572d71eee0e9dc580e21
Gabriel Henrique Coelho Freitas <coelho.tibbers@gmail.com>
2025-05-06 06:53:38 -0300

    feat: adicionar componente BasicTable com estilo e dados iniciais

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "document/components/navbar";
 import style from "./page.module.css";
+import BasicTable from "document/components/table";
 
 export default function Home() {
   return (
     <div className={style.container}>
       <Navbar />
+      <BasicTable />
     </div>
   );
 }
